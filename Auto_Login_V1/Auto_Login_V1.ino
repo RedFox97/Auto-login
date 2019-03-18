14f7b009438b5a5e8902ecc7954a98ffaff05f3b
Pham Van Hieu <pvhieu9702@gmail.com>
2019-03-18 10:33:48 +0700

    Update Auto_Login_V1.ino
    Hoàn thành

--- Auto_Login_V1/Auto_Login_V1.ino
+++ Auto_Login_V1/Auto_Login_V1.ino
@@ -10,9 +10,7 @@
 #define desktop     4
 #define fb          5
 #define google      6
-#define ledDesktop  7
-#define ledFB       8
-#define ledGG       9
+#define led         7
 
 String passDesktop  = "Pass Desktop";
 String passFB       = "Pass Facebook";
@@ -21,16 +19,14 @@ unsigned long lastDebounceTimeDesktop, lastDebounceTimeFB, lastDebounceTimeGG;
 unsigned long debounceDelay = 100;
 int lastButtonStateDesktop, lastButtonStateFB, lastButtonStateGG;
 int buttonStateDesktop, buttonStateFB, buttonStateGG;
-int ledDesktopState, ledFBState, ledGoogleState;
+int ledState = LOW;
 
 void setup(){
   Serial.begin(9600);
   pinMode(desktop, INPUT_PULLUP);
   pinMode(fb, INPUT_PULLUP);
   pinMode(google, INPUT_PULLUP);
-  pinMode(ledDesktop, OUTPUT);
-  pinMode(ledFB, OUTPUT);
-  pinMode(ledGG, OUTPUT);
+  pinMode(led, OUTPUT);
 }
 
 void loop() {
@@ -41,13 +37,11 @@ void loop() {
     if (digitalRead(desktop) != buttonStateDesktop) {           // Kiểm tra trạng thái hiện tại với trạng thái nút bấm lần bấm trước
       buttonStateDesktop = digitalRead(desktop);                // Lưu trạng thái bấm nút mới
       if (buttonStateDesktop == HIGH) {                         // Nếu nút đã được nhả thì thay đổi trạng thái led và gõ mật khẩu
-        ledDesktopState = !ledDesktopState;
         Keyboard.println(passDesktop);
         Keyboard.releaseAll();
       }
     }
   }
-  digitalWrite(ledDesktop, ledDesktopState);
   lastButtonStateDesktop = digitalRead(desktop);                // Liên tục cập nhật trạng thái nút nhấn
   
 /*--------------------------------------Đối với 2 nút còn lại làm tương tự ----------------------------------------*/
@@ -58,14 +52,13 @@ void loop() {
     if (digitalRead(fb) != buttonStateFB) {
       buttonStateFB = digitalRead(fb);
       if (buttonStateFB == HIGH) {                            
-        ledFBState = !ledFBState;
         Keyboard.println(passFB);
         Keyboard.releaseAll();
       }
     }
   }
-  digitalWrite(ledFB, ledFBState);
   lastButtonStateFB = digitalRead(fb);
+  
 /*-----------------------------------------------------------------------------------------------------------------*/
   if (digitalRead(google) != lastButtonStateGG) {
     lastDebounceTimeGG = millis();
@@ -74,12 +67,16 @@ void loop() {
     if (digitalRead(google) != buttonStateGG) {
       buttonStateGG = digitalRead(google);
       if (buttonStateGG == HIGH) {                            
-        ledGoogleState = !ledGoogleState;
         Keyboard.println(passGG);
         Keyboard.releaseAll();
       }
     }
   }
-  digitalWrite(ledGG, ledGoogleState);
   lastButtonStateGG = digitalRead(google);
+  
+/*-----------------------------------------------------------------------------------------------------------------*/
+  if (digitalRead(desktop) == 0 || digitalRead(fb) == 0 || digitalRead(google) == 0)
+    digitalWrite(led, HIGH);
+  else
+    digitalWrite(led, LOW);
 }
